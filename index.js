4249f4280b59e37f73c2610fc3154ac47f751fb7
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-09-28 19:57:47 -0300

    40

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ const app = express()
 app.get('/',(req,res)=>{
     res.send('hi there')
 })
-app.listen(8080,()=> console.log('listening on 8080'))
+app.listen(8080,()=> {
+    console.log('listening on 8080')
+})
